78a3e3eddffc32d37176a6f457411e169320b7bb
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-03-17 21:38:56 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -109,3 +109,140 @@ const close_iframe = () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Defining different IDs
+const heading = document.getElementById("event_heading_1");
+const detail = document.getElementById("event_detail_1");
+const btn = document.getElementById("register_btn_1");
+const forward = document.getElementById("forward_btn_1");
+const backward = document.getElementById("backward_btn_1");
+var arr1 = 0, arr_num_1 = 0;
+
+//Linking with API
+const request_1 = new XMLHttpRequest();
+request_1.open('GET','https://raw.githubusercontent.com/raghavdhingra/Gates_2k19/master/apis/misc_metadata.json');
+
+//request_1
+const miscevent = () => {
+    arr1 = JSON.parse(request_1.responseText);
+    heading.innerHTML = arr1[arr_num_1].name;
+    detail.innerHTML = arr1[arr_num_1].data;
+    btn.href = arr1[arr_num_1].link;
+    document.body.style.overflowY="hidden";
+    document.getElementById("iframe_1").style.zIndex='11';
+    document.getElementById("iframe_1").style.opacity='1';
+    document.getElementById("total_event_1").innerText=JSON.parse(request_1.responseText).length;
+}
+
+forward.addEventListener("click",()=>{
+  arr1 = JSON.parse(request_1.responseText);
+  if (arr_num_1 === arr1.length-1){
+      arr_num_1 = 0;
+      heading.innerHTML = arr1[arr_num_1].name;
+      detail.innerHTML = arr1[arr_num_1].data;
+      btn.href = arr1[arr_num_1].link;
+  }
+  else{
+      arr_num_1+=1;
+      heading.innerHTML = arr1[arr_num_1].name;
+      detail.innerHTML = arr1[arr_num_1].data;
+      btn.href = arr1[arr_num_1].link;
+  }
+  document.getElementById("current_event_1").innerText=arr_num_1+1;
+});
+backward.addEventListener("click",()=>{
+  arr1 = JSON.parse(request_1.responseText);
+  if (arr_num_1 === 0){
+      arr_num_1 = arr1.length - 1;
+      heading.innerHTML = arr1[arr_num_1].name;
+      detail.innerHTML = arr1[arr_num_1].data;
+      btn.href = arr1[arr_num_1].link;
+  }
+  else {
+      arr_num_1-=1;
+      heading.innerHTML = arr1[arr_num_1].name;
+      detail.innerHTML = arr1[arr_num_1].data;
+      btn.href = arr1[arr_num_1].link;
+  }
+  document.getElementById("current_event_1").innerText=arr_num_1+1;
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Defining different IDs
+//request_2
+
+const heading_2 = document.getElementById("event_heading_2");
+const detail_2 = document.getElementById("event_detail_2");
+const btn_2 = document.getElementById("register_btn_2");
+const forward_2 = document.getElementById("forward_btn_2");
+const backward_2 = document.getElementById("backward_btn_2");
+var arr2 = 0, arr_num_2 = 0;
+
+
+const request_2 = new XMLHttpRequest();
+request_2.open('GET','https://raw.githubusercontent.com/raghavdhingra/Gates_2k19/master/apis/tech_metadata.json');
+
+const techevent = () => {
+  arr2 = JSON.parse(request_2.responseText);
+  heading_2.innerHTML = arr2[arr_num_2].name;
+  detail_2.innerHTML = arr2[arr_num_2].data;
+  btn_2.href = arr2[arr_num_2].link;
+  document.body.style.overflowY="hidden";
+  document.getElementById("iframe_2").style.zIndex='11';
+  document.getElementById("iframe_2").style.opacity='1';
+  document.getElementById("total_event_2").innerText=JSON.parse(request_2.responseText).length;
+}
+
+forward_2.addEventListener("click",()=>{
+  arr2 = JSON.parse(request_2.responseText);
+  if (arr_num_2 === arr2.length-1){
+      arr_num_2 = 0;
+      heading_2.innerHTML = arr2[arr_num_2].name;
+      detail_2.innerHTML = arr2[arr_num_2].data;
+      btn_2.href = arr2[arr_num_2].link;
+  }
+  else{
+      arr_num_2+=1;
+      heading_2.innerHTML = arr2[arr_num_2].name;
+      detail_2.innerHTML = arr2[arr_num_2].data;
+      btn_2.href = arr2[arr_num_2].link;
+  }
+  document.getElementById("current_event_2").innerText=arr_num_2+1;
+});
+backward_2.addEventListener("click",()=>{
+  arr2 = JSON.parse(request_2.responseText);
+  if (arr_num_2 === 0){
+      arr_num_2 = arr2.length - 1;
+      heading_2.innerHTML = arr2[arr_num_2].name;
+      detail_2.innerHTML = arr2[arr_num_2].data;
+      btn_2.href = arr2[arr_num_2].link;
+  }
+  else {
+      arr_num_2-=1;
+      heading_2.innerHTML = arr2[arr_num_2].name;
+      detail_2.innerHTML = arr2[arr_num_2].data;
+      btn_2.href = arr2[arr_num_2].link;
+  }
+  document.getElementById("current_event_2").innerText=arr_num_2+1;
+});
+
+
+
